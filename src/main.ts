0427f39351526db77212a7321b45fb2939af9b88
GRRNebula2 <pahkina.katainen@protonmail.com>
2026-03-13 12:38:34 +0200

    tausta-layerit 1 ja 2 kaboomin addilla

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import type { KaboomCtx } from "kaboom";
 import k from "./kaboomCtx";
 
-function arena(k: KaboomCtx) {
+k.loadSprite(
+    "background-layer-1",
+    "./assets/background/background_layer_1.png"
+);
 
+k.loadSprite(
+    "background-layer-2",
+    "./assets/background/background_layer_2.png"
+);
+
+function arena(k: KaboomCtx) {
+    k.add([k.sprite("background-layer-1"), k.pos(0, 0), k.scale(4), k.fixed()]);
+    k.add([k.sprite("background-layer-2"), k.pos(0, 0), k.scale(4), k.fixed()]);
 }
 
 k.scene("arena", () => arena(k));
+
+k.go("arena");
